38006d087b3c988dd8120dc030acdb73c143d133
Noa Virellia <noa@requiem.garden>
2026-05-02 22:06:39 +0800

    update ghostty and fish config

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -2,11 +2,18 @@ set fish_greeting
 
 starship init fish | source
 zoxide init fish | source
+atuin init fish --disable-up-arrow | source
 
 fish_add_path ~/.local/bin
-fish_add_path /opt/nvim-linux-x86_64/bin
 
 alias n='nvim'
 alias gg='lazygit'
+alias cl='claude --dangerously-skip-permissions'
+
+if test -e /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.fish
+    source /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.fish
+end
+
+fish_add_path ~/.nix-profile/bin
 
 set -x SSH_AUTH_SOCK "~/.1password/agent.sock"
