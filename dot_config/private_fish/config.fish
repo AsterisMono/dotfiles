09371475f999773dcabe0fbaa381e9072b28b268
Noa Virellia <noa@requiem.garden>
2026-05-27 21:00:35 +0800

    add yazi config

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -17,4 +17,13 @@ end
 
 fish_add_path ~/.nix-profile/bin
 
+function y
+  set tmp (mktemp -t "yazi-cwd.XXXXXX")
+  command yazi $argv --cwd-file="$tmp"
+  if read -z cwd < "$tmp"; and [ "$cwd" != "$PWD" ]; and test -d "$cwd"
+    builtin cd -- "$cwd"
+  end
+  command rm -f -- "$tmp"
+end
+
 set -x SSH_AUTH_SOCK "~/.1password/agent.sock"
